dbd0196df778e7f3f57bee0b2f19bc2f31b1331e
Nakul Sabharwal <nasabhar@microsoft.com>
2019-03-11 16:30:05 +0530

    Implemented msgraph-sdk-java's IAuthProvider interface. Migrated core authenticate request to okhttp.

--- src/test/java/com/microsoft/graph/auth/confidentialClient/AuthorizationCodeProviderTests.java
+++ src/test/java/com/microsoft/graph/auth/confidentialClient/AuthorizationCodeProviderTests.java
@@ -6,7 +6,6 @@ import static org.junit.Assert.assertNotNull;
 import java.util.Arrays;
 import java.util.List;
 
-import org.apache.http.client.methods.HttpGet;
 import org.apache.oltu.oauth2.client.request.OAuthClientRequest;
 import org.apache.oltu.oauth2.common.exception.OAuthProblemException;
 import org.apache.oltu.oauth2.common.exception.OAuthSystemException;
@@ -19,10 +18,12 @@ import org.mockito.junit.MockitoJUnitRunner;
 import com.microsoft.graph.auth.AuthConstants;
 import com.microsoft.graph.auth.enums.NationalCloud;
 
+import okhttp3.Request;
+
 @Ignore
 @RunWith(MockitoJUnitRunner.class)
 public class AuthorizationCodeProviderTests {
-	
+
 	private  String CLIENT_ID = "CLIENT_ID";
 	private  String REDIRECT_URL = "http://localhost";
 	private  String SECRET = "CLIENT_SECRET";
@@ -30,13 +31,14 @@ public class AuthorizationCodeProviderTests {
 	private String AUTHORIZATION_CODE = "AUTHORIZATION_CODE";
 	private NationalCloud NATIONAL_CLOUD = NationalCloud.Global;
 	private String TENANT = AuthConstants.Tenants.Common;
-	
+	private String testmeurl = "https://graph.microsoft.com/v1.0/me/"; 
+
 	@Test
 	public void getAuthorizationCodeProviderTest() {
 		AuthorizationCodeProvider authorizationCodeProvider = new AuthorizationCodeProvider(CLIENT_ID, SCOPES, AUTHORIZATION_CODE, REDIRECT_URL, SECRET);
 		assertNotNull(authorizationCodeProvider);
 	}
-	
+
 	@Test
 	public void getAuthorizationCodeProviderWithNationalCloudTenantTest() {
 		AuthorizationCodeProvider authorizationCodeProvider = new AuthorizationCodeProvider(CLIENT_ID, SCOPES, AUTHORIZATION_CODE, REDIRECT_URL, NATIONAL_CLOUD, TENANT, SECRET);
@@ -50,32 +52,32 @@ public class AuthorizationCodeProviderTests {
 		OAuthClientRequest request = authorizationCodeProvider.getTokenRequestMessage(AUTHORIZATION_CODE);
 		assertEquals(expected, request.getBody().toString());
 	}
-	
+
 	@Test
 	public void authenticateRequestTest() throws OAuthSystemException, OAuthProblemException {
 		AuthorizationCodeProvider authorizationCodeProvider = Mockito.mock(AuthorizationCodeProvider.class);
 		Mockito.when(authorizationCodeProvider.getTokenRequestMessage(AUTHORIZATION_CODE)).thenReturn(Mockito.mock(OAuthClientRequest.class));
-		HttpGet httpget = new HttpGet("https://graph.microsoft.com/v1.0/me/");
+		Request httpget = new Request.Builder().url(testmeurl).build();
 		authorizationCodeProvider.authenticateRequest(httpget);
-		String actual = httpget.getFirstHeader("Authorization").getValue();
+		String actual = httpget.header("Authorization");
 		assertEquals("Bearer test_accessToken"	, actual);
 	}
-	
+
 	@Test
 	public void getAccessTokenNewRequestTest() throws OAuthSystemException, OAuthProblemException {
 		AuthorizationCodeProvider authorizationCodeProvider = new AuthorizationCodeProvider(CLIENT_ID, SCOPES, AUTHORIZATION_CODE, REDIRECT_URL, SECRET);
-		HttpGet httpget = new HttpGet("https://graph.microsoft.com/v1.0/me/");
+		Request httpget = new Request.Builder().url(testmeurl).build();
 		authorizationCodeProvider.authenticateRequest(httpget);
-		String actualTokenparameter = httpget.getFirstHeader("Authorization").getValue();
+		String actualTokenparameter = httpget.header("Authorization");
 		assertNotNull(actualTokenparameter);
 	}
-	
+
 	@Test
 	public void getAccessTokenNewRequestWithNationalCloudTenantTest() throws OAuthSystemException, OAuthProblemException {
 		AuthorizationCodeProvider authorizationCodeProvider = new AuthorizationCodeProvider(CLIENT_ID, SCOPES, AUTHORIZATION_CODE, REDIRECT_URL, NATIONAL_CLOUD, TENANT, SECRET);
-		HttpGet httpget = new HttpGet("https://graph.microsoft.com/v1.0/me/");
+		Request httpget = new Request.Builder().url(testmeurl).build();
 		authorizationCodeProvider.authenticateRequest(httpget);
-		String actualTokenparameter = httpget.getFirstHeader("Authorization").getValue();
+		String actualTokenparameter = httpget.header("Authorization");
 		assertNotNull(actualTokenparameter);
 	}
 
